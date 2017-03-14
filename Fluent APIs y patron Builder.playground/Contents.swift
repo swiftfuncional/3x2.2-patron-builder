19df7dd88c9e752afd9275a6da3e299b941b064a
Swift Funcional <info@swiftfuncional.com>
2017-03-14 15:14:46 +0100

    Second class ConnectorBuilder with two setters (for host and port) and one build method

--- Fluent APIs y patron Builder.playground/Contents.swift
+++ Fluent APIs y patron Builder.playground/Contents.swift
@@ -2,7 +2,7 @@ class Connector {
 	let host: String?
 	let port: Int?
 
-	init(host: String?, port: Int) {
+	init(host: String?, port: Int?) {
 		self.host = host
 		self.port = port
 	}
@@ -11,3 +11,23 @@ class Connector {
 
 	}
 }
+
+class ConnectorBuilder {
+	var connector = Connector(host: nil, port: nil)
+
+	func set(host: String) -> Self {
+		connector = Connector(host: host, port: connector.port)
+
+		return self
+	}
+
+	func set(port: Int) -> Self {
+		connector = Connector(host: connector.host, port: port)
+
+		return self
+	}
+
+	func build() -> Connector {
+		return connector
+	}
+ }
